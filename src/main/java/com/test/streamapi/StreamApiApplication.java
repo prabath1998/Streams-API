3d5feaeee8d1e35720b9e881dd922a0443a568df
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-03 23:04:33 +0530

    add map,filter,collect operations

--- src/main/java/com/test/streamapi/StreamApiApplication.java
+++ src/main/java/com/test/streamapi/StreamApiApplication.java
@@ -3,11 +3,47 @@ package com.test.streamapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class StreamApiApplication {
 
+	static List<Employee> employees = new ArrayList<>();
+	static {
+		employees.add(new Employee("prabath","udayanga",50000.0, List.of("project 1","project 2")));
+		employees.add(new Employee("janaka","chathuranga",60000.0, List.of("project 1","project 3")));
+		employees.add(new Employee("prabath","udayanga",65000.0, List.of("project 3","project 4")));
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(StreamApiApplication.class, args);
+//		SpringApplication.run(StreamApiApplication.class, args);
+		//foreach
+		employees.stream()
+				.forEach(employee -> System.out.println(employee));
+		//map
+		//collect
+		List<Employee> increasedSalary = employees.stream()
+				.map(employee -> new Employee(
+						employee.getFirstName(),
+						employee.getLastName(),
+						employee.getSalary() * 1.10,
+						employee.getProjects()
+				)).collect(Collectors.toList());
+		System.out.println(increasedSalary);
+
+		//filter
+		List<Employee> filterEmployee = employees.stream()
+				.filter(employee -> employee.getSalary() > 59000.0)
+				.map(employee -> new Employee(
+						employee.getFirstName(),
+						employee.getLastName(),
+						employee.getSalary() * 1.10,
+						employee.getProjects()
+				)).collect(Collectors.toList());
+		System.out.println(filterEmployee);
 	}
 
 }
