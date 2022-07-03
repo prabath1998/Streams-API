e4531b970c372a5b9ad4ad22cf6c6a8f1121e3a2
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-03 22:39:37 +0530

    Initial commit

---
+++ src/test/java/com/test/streamapi/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
